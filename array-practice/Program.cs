f2cd7f0442fb78ef413c9cecd96d69c310c3d15f
1 <1@LAPTOP-ILGMJ5ES>
2023-03-06 01:11:13 +0300

    Добавьте файлы проекта.

---
+++ array-practice/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace array_practice
+{
+    class Program
+    { 
+    static void Main(string[] args) 
+        {
+            Console.Write("Введите сумму вклада: ");
+            decimal sum = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Введите срок вклада в месяцах:");
+            int mounth = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= mounth; i++)
+            { 
+            sum+=sum*0.07m;
+            }
+            Console.WriteLine($"Спустя {mounth} месяцев сумма вклада составит {sum }");
+            Console.ReadKey();
+        } 
+    }
+}
---
+++ practice2/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите сумму вклада: ");
+        decimal sum = Convert.ToDecimal(Console.ReadLine());
+        Console.Write("Введите срок вклада в месяцах:");
+        int mounth = Convert.ToInt32(Console.ReadLine());
+        int i = mounth;
+        while (i > 0)
+        {
+            sum += sum * 0.07m;
+            i--;
+        }
+        Console.WriteLine($"Спустя {mounth} месяцев сумма вклада составит {sum }");
+        Console.ReadKey();
+    }
+}
